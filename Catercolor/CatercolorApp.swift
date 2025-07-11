12cb12e4a42289b10e03ad03e7c10c2a899b75fe
jennifer.warbeck <jenx.wa@gmail.com>
2025-07-11 02:29:11 +0200

    Initial Commit

---
+++ Catercolor/CatercolorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatercolorApp.swift
+//  Catercolor
+//
+//  Created by Jennifer Warbeck on 11.07.25.
+//
+
+import SwiftUI
+
+@main
+struct CatercolorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatercolorTests/CatercolorTests.swift
@@ -0,0 +1,16 @@
+//
+//  CatercolorTests.swift
+//  CatercolorTests
+//
+//  Created by Jennifer Warbeck on 11.07.25.
+//
+
+import Testing
+
+struct CatercolorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
